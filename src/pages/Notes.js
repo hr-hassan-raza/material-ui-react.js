8e533e5295bcc8737cddc4e28369d52c7305e716
hr_hassan_raza <hraza20000@gmail.com>
2022-02-20 17:17:00 +0500

    data fetching-json-server

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -8,10 +8,18 @@ import NoteCard from "../components/NoteCard";
 export default function Notes() {
   const [notes, setNotes] = useState([]);
   useEffect(() => {
-    fetch("http://localhost:8000/2018146")
+    fetch("http://localhost:8000/notes")
       .then((res) => res.json())
       .then((data) => setNotes(data));
   }, []);
+
+  const handleDelete = async (id) => {
+    await fetch("http://localhost:8000/notes" + id, {
+      method: "DELETE",
+    });
+    const newNotes = notes.filter();
+  };
+
   return (
     <Container>
       {/* <Grid container>
